e93803226823510845e4ff4f01a63e6c0546d04f
GGB <GGB@DESKTOP-5GSO0SP>
2025-08-01 01:09:11 +0800

    Initial commit

---
+++ PetHomeAPI/PetHomeAPI/Controllers/LoginController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PetHomeAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpGet]
+        public void Get()
+        {
+
+        }
+    }
+}
